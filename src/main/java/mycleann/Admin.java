50e66372500c711bbc37a6526aa46423e075959b
Hp <Hp@172.19.8.29>
2023-05-08 16:04:26 +0300

    zaq

--- src/main/java/mycleann/Admin.java
+++ src/main/java/mycleann/Admin.java
@@ -9,21 +9,20 @@ public class Admin
 	Properties r = new Properties();
 	InputStream input888 = null;
 	protected boolean adminlogged;
-	 protected String admin_name, admin_password;
+	protected String admin_name, admin_password;
 	 protected String Update;
 	 public Admin(String user_name, String password) 
 	 {
-		this.admin_name.equals(user_name);
-		 this.admin_password.equals(password);
+		 this.admin_name=user_name;
+		 this.admin_password=password;
 		 
 		 this.adminlogged=false;
 	 }
 	 public Admin() 
 	 {
-		this.admin_name="adham_12028277";
-		this.admin_password="123456789@adham";				 
-//		 r.getProperty("dbname");
-//		 r.getProperty("dbpassword");
+		 this.admin_name="adham_12028277";
+		 this.admin_password="123456789@adham";				 
+				 
 	 }
 
 public boolean login(String user_name, String password)
