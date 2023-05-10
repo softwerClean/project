c5d6299c1cd245b06b55a03e9d3e4403aec6d11d
Eng. Adham-AboYaqoub <Eng. Adham-AboYaqoub@Eng-Adham-AboYaqoub>
2023-05-10 22:49:56 +0300

    d

--- src/main/java/mycleann/FirstClass.java
+++ src/main/java/mycleann/FirstClass.java
@@ -331,7 +331,8 @@ static String filenameproperities="customer.txt";
 	                                    String nameInFile7 = parts7[1];
 	                                    if (nameInFile7.equals(name7)) {
 	                                        fos7.write((parts7[0] + " " + parts7[1] + " " + parts7[2] + " " + parts7[3] + " " + parts7[4] + "\n").getBytes());
-	                                        logger.log(Level.INFO, String.format("%s %s %s %s %s %s%n", parts7[0], "%s", parts7[1], "%s", parts7[2], "%s", parts7[3], "%s", parts7[4], "%s"), "");
+	                                        String logMessage = String.format("%s %s %s %s %s %s%n", parts7[0], "%s", parts7[1], "%s", parts7[2], "%s", parts7[3], "%s", parts7[4], "%s");
+	                                        logger.log(Level.INFO, logMessage);
 	                                    }
 
 	                                    chunk7 = chunk7.substring(index7 + 1);
