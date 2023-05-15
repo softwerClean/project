6bb1703276dc5c4e343cc4e353d51953f0d8a83d
MuheebHasan <124196739+MuheebHasan@users.noreply.github.com>
2023-05-15 14:54:57 +0300

    Update EmailSenderTest.java

--- src/test/java/project2023_test/EmailSenderTest.java
+++ src/test/java/project2023_test/EmailSenderTest.java
@@ -4,19 +4,20 @@ import org.junit.Test;
 
 import mycleann.EmailSenderrr;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 
 public class EmailSenderTest {
-    @Test
-    public void testSendEmail() throws AddressException, MessagingException {
-        EmailSenderrr emailSender = new EmailSenderrr("smtp.gmail.com", "587", "adham yaqoub", "0594348312Amamry");
-        emailSender.sendEmail("amamry2021.2002@gmail.com", "hello");
+	@Test
+	public void testSendEmail() throws AddressException, MessagingException {
+	    EmailSenderrr emailSender = new EmailSenderrr("smtp.gmail.com", "587", "adham yaqoub", "0594348312Amamry");
+	    emailSender.sendEmail("amamry2021.2002@gmail.com", "hello");
 
-        assertEquals("Email should have been sent successfully", emailSender.isSentSuccessfully());
-       
-    }
+	    assertTrue("Email should have been sent successfully", emailSender.isSentSuccessfully());
+	}
+
 
     
 }
